46601cfea02430ad6b731c0018aed9da87b7a24b
BenediktusG <benediktus.gianto.jarod@gmail.com>
2025-06-23 23:34:31 +0800

    fix edit data bug

--- src/route/api.js
+++ src/route/api.js
@@ -18,7 +18,7 @@ userRouter.patch('/auth/password', userController.changePassword);
 userRouter.post('/data', dataController.register);
 userRouter.get('/data', dataController.get);
 userRouter.get('/data/:dataId', dataController.getDataWithId);
-userRouter.patch('/data/:dataId', dataController.update);
+userRouter.put('/data/:dataId', dataController.update);
 userRouter.delete('/data/:dataId', dataController.deleteData);
 
 // Log API
