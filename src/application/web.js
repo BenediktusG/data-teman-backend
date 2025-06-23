72eaacf18b2d8a106d17c5d234812170e957f4fd
BenediktusG <benediktus.gianto.jarod@gmail.com>
2025-06-23 12:40:11 +0800

    add https configuration

--- src/application/web.js
+++ src/application/web.js
@@ -4,6 +4,13 @@ import { errorMiddleware } from '../middleware/error-middleware.js';
 import cookieParser from 'cookie-parser';
 import { userRouter } from '../route/api.js';
 import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url'; 
+
+const __filename = fileURLToPath(import.meta.url); // Get current file path
+const __dirname = path.dirname(__filename);
 
 export const web = express();
 
@@ -18,6 +25,24 @@ web.use(publicRouter);
 web.use(userRouter);
 web.use(errorMiddleware);
 
-web.listen(process.env.APP_PORT, () => {
+const certPath = path.join(__dirname, '..', '..' ,'certs', 'localhost.pem');
+const keyPath = path.join(__dirname, '..', '..', 'certs', 'localhost-key.pem');
+
+if (!fs.existsSync(certPath) || !fs.existsSync(keyPath)) {
+  console.error('Error: SSL certificate or key file not found!');
+  console.error(`Expected cert: ${certPath}`);
+  console.error(`Expected key: ${keyPath}`);
+  console.error('Please make sure you have run mkcert and placed the files correctly.');
+  process.exit(1);
+};
+
+const sslOptions = {
+    key: fs.readFileSync(keyPath, 'utf8'),
+    cert: fs.readFileSync(certPath, 'utf8'),
+};
+
+const httpsServer = https.createServer(sslOptions, web);
+
+httpsServer.listen(process.env.APP_PORT, () => {
     console.log('Application is running');
 });
