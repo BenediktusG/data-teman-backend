d13299d0e7dd2a5532b599c85aefd7fc33c6fde5
fajri-farid <fajrijry14@gmail.com>
2025-06-27 03:52:55 +0800

    feat: require description and photoLink in createDataValidation

--- src/validation/data-validation.js
+++ src/validation/data-validation.js
@@ -2,10 +2,10 @@ import Joi from 'joi';
 
 export const createDataValidation = Joi.object({
     name: Joi.string().min(1).max(191).required(),
-    description: Joi.string().min(1).max(191),
+    description: Joi.string().min(1).max(191).required(),
     address: Joi.string().min(1).max(191).required(),
     birthDate: Joi.date().required(),
-    photoLink: Joi.string().uri(),
+    photoLink: Joi.string().uri().required(),
 });
 
 export const updateDataValidation = Joi.object({
